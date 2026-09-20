2b6019c2c3f8accbef9b9ee06992120c39769b70
neurosoul <znbtonyt@gmail.com>
2026-09-20 18:25:26 -0400

    Add header comment and introduction lines

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+* Name: neuro
+* Course: CSCI 1250, Section 001
+* Assignment: Lab 0, Hello World
+* Date: September 20, 2026
+* Description: Prints a greeting and a short introduction to the console.
+*/
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is neuro.");
+Console.WriteLine("This is my first program at ETSU.");
